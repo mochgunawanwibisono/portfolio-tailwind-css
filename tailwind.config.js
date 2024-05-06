dc80f55cb0f82ef7fafba7d9eef1a495cfbd96d0
Gunawan IT <gunawan.itdepartemen@gmail.com>
2024-05-06 14:39:21 +0700

    Update Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  // content: ['./public/**/*.{html,js}'],
+  content: ['./index.html'],
   theme: {
     container:{
       center:true,
